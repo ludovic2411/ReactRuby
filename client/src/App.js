fe5947520e26601acc3bf79a353c82257eba1cd0
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-01-28 16:01:49 +0100

    sign_i+materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route,Switch, Link } from "react-router-dom";
+import Home from './home';
+import signIn from './sign_in';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/sign_in">Sign in</Link>
+            </li>
+          </ul>
+          <switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/sign_in" component={signIn}/>
+          </switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
