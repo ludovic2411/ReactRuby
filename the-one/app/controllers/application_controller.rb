bc9e9804e1f878a70ffb58ab6d61c271147c301e
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-01-28 13:31:29 +0100

    todos

--- the-one/app/controllers/application_controller.rb
+++ the-one/app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::API
   rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
   private
+
+  def current_user
+   @current_user ||= User.find(payload['user_id'])
+  end
   
   def not_authorized
     render json: { error: 'Not authorized' }, status: :unauthorized
-end
+  end
 
 end
